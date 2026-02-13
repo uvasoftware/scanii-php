024583d458480f57cc5dbf76e56d373e2cab704c
Rafael Ferreira <raf@uvasoftware.com>
2026-02-13 06:09:43 -0500

    Bumped version and added CA1 endpoint

--- src/Scanii/ScaniiTarget.php
+++ src/Scanii/ScaniiTarget.php
@@ -10,5 +10,6 @@ abstract class ScaniiTarget
   const EU2 = 'https://api-eu2.scanii.com/v2.1';
   const AP1 = 'https://api-ap1.scanii.com/v2.1';
   const AP2 = 'https://api-ap2.scanii.com/v2.1';
+  const CA1 = 'https://api-ca1.scanii.com/v2.1';
 }
 
